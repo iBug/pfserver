c7de2c998d9d3c8f6859d1ef119ef615b4d9d00e
iBug <git@ibugone.com>
2020-03-10 23:12:54 +0800

    logd: differentiate values and flags

--- logd.rb
+++ logd.rb
@@ -9,24 +9,29 @@ module LogD
       @file = file
     end
 
-    def run &block
+    def run
       IO.popen(["tail", "-F", @file]).each_line do |line|
         line.chomp!
         next if line.scan(KEY_RE).empty?
         pref, content = line.split KEY_RE, 2
         time = Time.parse pref
-        data = {}
+        data = {:flags => Set.new}
         content.split.each do |item|
           key, value = item.split "="
-          data[key] = value || ''
+          if value.nil?
+            data[:flags] << key
+          else
+            data[key] = value
+          end
         end
         yield (time, data)
       end
     end
 
-    def run_background &block
+    def run_background
       Thread.new do
         run &block
       end
+    end
   end
 end
